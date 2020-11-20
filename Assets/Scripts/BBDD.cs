b433422c14c8388f6041601694f06df0ca13fa09
MarcGasparGuardiola <73303963+MarcGasparGuardiola@users.noreply.github.com>
2020-11-20 16:45:01 +0100

    Correct Select from DDBB

--- Assets/Scripts/BBDD.cs
+++ Assets/Scripts/BBDD.cs
@@ -7,25 +7,28 @@ using System.Data;
 
 public class BBDD : MonoBehaviour
 {
+    //La connexio pot ser statica ja que sempre sera la mateixa per totes les connexions
+    public string conn = "URI=file:" + Application.dataPath + "/baseDadesSQLitle.db"; //Path to database.
     public void SelectTest() 
     {
-        Debug.Log(Application.dataPath);
-
-        string conn = "URI=file:" + Application.dataPath + "/baseDadesSQLitle.db"; //Path to database.
         IDbConnection dbconn;
         dbconn = (IDbConnection)new SqliteConnection(conn);
         dbconn.Open(); //Open connection to the database.
         IDbCommand dbcmd = dbconn.CreateCommand();
-        string sqlQuery = "SELECT value,name, randomSequence " + "FROM PlaceSequence";
+        string sqlQuery = "SELECT ID_User,mail, password, height, weight, maxFC, maxW  " + "FROM user";
         dbcmd.CommandText = sqlQuery;
         IDataReader reader = dbcmd.ExecuteReader();
         while (reader.Read())
         {
-            int value = reader.GetInt32(0);
-            string name = reader.GetString(1);
-            int rand = reader.GetInt32(2);
+            int id = reader.GetInt32(0);
+            string mail = reader.GetString(1);
+            string password = reader.GetString(2);
+            int height = reader.GetInt32(3);
+            int weight = reader.GetInt32(4);
+            int maxFC = reader.GetInt32(5);
+            int maxW = reader.GetInt32(6);
 
-            Debug.Log("value= " + value + "  name =" + name + "  random =" + rand);
+            Debug.Log("id= " + id + "  email=" + mail + "  password=" + password + "  height=" + height + "  weight=" + weight + "  maxFC=" + maxFC + "  maxW=" + maxW);
         }
         reader.Close();
         reader = null;
@@ -35,6 +38,27 @@ public class BBDD : MonoBehaviour
         dbconn = null;
     }
 
+
+    public void addUser() {
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "INSERT INTO cars(ID_User,mail, password, height, weight, maxFC, maxW) VALUES(, @price)";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        cmd.CommandText = "INSERT INTO cars(name, price) VALUES(@name, @price)";
+
+        cmd.Parameters.AddWithValue("@name", "BMW");
+        cmd.Parameters.AddWithValue("@price", 36600);
+        cmd.Prepare();
+
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine("row inserted");
+    }
 
     // Update is called once per frame
     void Update()
