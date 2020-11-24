9ca37cf28e7f8c44918b329d26260f381750fe62
MarcGasparGuardiola <73303963+MarcGasparGuardiola@users.noreply.github.com>
2020-11-24 11:15:23 +0100

    Created User script

---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    private int id { get;}
+    private string mail { get; set; }
+    private string password { get; set; }
+    private int height { get; set; }
+    private int weight { get; set; }
+    private int maxFC { get; set; }
+    private int maxW { get; set; }
+    //private ArrayList<Workout>;
+
+    public User(int id, string mail, string password, int height, int weight, int maxFC, int maxW)
+    {
+        this.id = id;
+        this.mail = mail;
+        this.password = password;
+        this.height = height;
+        this.weight = weight;
+        this.maxFC = maxFC;
+        this.maxW = maxW;
+    }
+
+    public int MyProperty 
+}
