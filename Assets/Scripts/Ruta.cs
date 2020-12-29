42c58ddc8cbfe7183465a4a8a20b8ab334fcc302
MarcGasparGuardiola <73303963+MarcGasparGuardiola@users.noreply.github.com>
2020-12-29 19:14:02 +0100

    Added slope calc function

--- Assets/Scripts/Ruta.cs
+++ Assets/Scripts/Ruta.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Ruta
 {
-    string name;
-    List<TrackPoint> trackPoints;
-    int poitiveElevation;
-    int negativeElevation;
-    float distance;
+    string name { get; set; }
+    List<TrackPoint> trackPoints { get; set; }
+    int poitiveElevation { get; set; }
+    int negativeElevation { get; set; }
+    float distance { get; set; }
+    string description { get; set; }
 
+    double[,] pendent { get; set; }
+
+    public Ruta(string name, List<TrackPoint> trackPoints, int poitiveElevation, int negativeElevation, float distance, double[,] pendent)
+    {
+        this.name = name;
+        this.trackPoints = trackPoints;
+        this.poitiveElevation = poitiveElevation;
+        this.negativeElevation = negativeElevation;
+        this.distance = distance;
+        this.pendent = pendent;
+    }
 }
--- Assets/Scripts/SeleccionRuta.cs
+++ Assets/Scripts/SeleccionRuta.cs
@@ -28,12 +28,20 @@ public class SeleccionRuta : MonoBehaviour
         string name = gpx.GetName(routePath);
         Debug.Log(name);
 
+        //calc Elevation
         Debug.Log(CalcPositiveElevation(trackPoints));
         Debug.Log(CalcNegativeElevation(trackPoints));
 
         //Dividiem entre 1000 ja que el resultat ens el dona en metres
-        Debug.Log(CalcDistance(trackPoints)/1000 + " KM");
-        //calc Elevation
+        double[] ditancePoints = CalcDistance(trackPoints);
+
+        Debug.Log(CalcTotalDistance(ditancePoints)/1000 + " KM");
+
+        //Calc pendent
+        double[] slope = CalcSlope(trackPoints, ditancePoints);
+
+        AddSlopeInformation(slope);
+
     }
 
     private double CalcPositiveElevation(List<TrackPoint> trackPoints)
@@ -93,22 +101,25 @@ public class SeleccionRuta : MonoBehaviour
         return negativeElev * -1;
     }
 
-    private double CalcDistance(List<TrackPoint> trackPoints)
+    private double[] CalcDistance(List<TrackPoint> trackPoints)
     {
         double baseLat = trackPoints[1].lat;
         double baseLon = trackPoints[1].lon;
         double targetLat = trackPoints[0].lat;
         double targetLon = trackPoints[0].lon;
 
+        //hem de restar 1 ja que volem la distancia entre dos punts i per tant hem de restar 1
+        double[] distance = new double[trackPoints.Count-1];
+
         //La primera crida la fem posant el target primer ja que aixi podem simplificar després l'assignació als nous punts
-        double distance = DistanceTo(targetLat, targetLon, baseLat, baseLon);
+        distance[0] = DistanceTo(targetLat, targetLon, baseLat, baseLon);
 
         for (int i = 2; i < trackPoints.Count; i++)
         {
             targetLat = trackPoints[i].lat;
             targetLon = trackPoints[i].lon;
 
-            distance += DistanceTo(baseLat, baseLon, targetLat, targetLon);
+            distance[i-1] = DistanceTo(baseLat, baseLon, targetLat, targetLon);
 
             baseLat = targetLat;
             baseLon = targetLon;
@@ -140,15 +151,112 @@ public class SeleccionRuta : MonoBehaviour
 
         return d;*/
 
+
+        //Fórmula treta de https://stackoverflow.com/questions/6366408/calculating-distance-between-two-latitude-and-longitude-geocoordinates
+        //És l'implementació de la funció GetCordinates de la classe GeoCoordinate
+
         var d1 = baseLat * (Math.PI / 180.0);
         var num1 = baseLon * (Math.PI / 180.0);
         var d2 = targetLat * (Math.PI / 180.0);
         var num2 = targetLon * (Math.PI / 180.0) - num1;
         var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
 
-        var d = 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
-        Debug.Log(d);
+        var d = Math.Round(6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3))), 2);
+        //Debug.Log(d);
         return d;
+    }
+
+    private double CalcTotalDistance(double[] points)
+    {
+        double dist = 0;
+        foreach (var point in points)
+        {
+            dist += point;
+        }
+
+        return dist;
+    }
+
+    private double[] CalcSlope(List<TrackPoint> trackPoints, double[] distancePoints)
+    {
+        double actualElev = trackPoints[0].ele;
+        double priorElev = trackPoints[5].ele;
+
+        double distanceBetweenPoints = Calc5pointsDistance(distancePoints, 0);
+
+        double[] slopePoints = new double[distancePoints.Length];
+
+        //* 100 ja que volem el percentatge per després poder adequar la dificultat del rodillo
+        slopePoints[0] = (priorElev - actualElev) / distanceBetweenPoints * 100;
+        Debug.Log(slopePoints[0]);
+        for (int i = 10; i < trackPoints.Count; i += 5)
+        {
+            actualElev = trackPoints[i].ele;
+            distanceBetweenPoints = Calc5pointsDistance(distancePoints, i-5);
+
+           
+
+            double elevDiff = Math.Round(actualElev - priorElev);
+
+            if (distanceBetweenPoints == 0)
+            {
+                slopePoints[i - 1] = 0;
+            }
+            else 
+            {
+                slopePoints[i - 1] = Math.Round(elevDiff / distanceBetweenPoints * 100, 2);
+            }
+          
+
+            //Debug.Log("I: " + i +" i-1: " +(i-1)+ " Target Point :" + actualElev + " Piror elev: " + priorElev + " Diff: " + elevDiff + " Dist between points: " + distanceBetweenPoints + " Slope: " + slopePoints[i - 1]);
+            //Debug.Log(slopePoints[i-1]);
+            priorElev = actualElev;
+        }
+        //Multipliquem per -1 per fer el número positiu
+        return slopePoints;
+    }
+
+    private double Calc5pointsDistance(double[] distancePoints, int index)
+    {
+        ArraySegment<double> splitarray = new ArraySegment<double>(distancePoints, index, 5);
+
+        double dist = 0;
+        foreach (var item in splitarray)
+        {
+            dist += item;
+        }
+
+        return dist;
+    }
+
+    private void AddSlopeInformation(double[] slope)
+    {
+        double actualElev = 0;
+        double priorElev = slope[0];
+
+        for (int i = 4; i < slope.Length; i += 5)
+        {
+           
+            actualElev = slope[i];
+            double elevDiff = Math.Round(actualElev - priorElev, 2);
+            double step = Math.Round(elevDiff / 4, 2);
+
+            Debug.Log("actual elev:" + actualElev + " prior elev:" + priorElev);
+
+            if (step != 0)
+            {
+                for (int j = i - 4; j < i; j++)
+                {
+                    slope[j] = Math.Round(slope[i] + step * (j - i), 1);
+                    Debug.Log("i:" + j + " value:" + slope[j]);
+                }
+                priorElev = actualElev;
+            }
+            Debug.Log("i:" + i + " value:" + slope[i]);
+
+
+
+        }
     }
 
 }
